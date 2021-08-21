018b81b7beb4ec8e82e6b07b93b46fb553a6c86b
ezap <ezap.ez98@gmail.com>
2021-08-21 00:40:19 -0500

    Two sums files

---
+++ two_sum_Maps.cpp
@@ -0,0 +1,28 @@
+class Solution{
+    public:
+        vector<int> twoSum(vector<int>& nums, int target){
+                
+        }
+};
+
+
+
+/*
+
+Using map 
+pick every element in an array and calulate the differnece  = target - pickedElement 
+
+check if the map has the diff value ans its index is not equal to the index of picked element 
+
+if the element is correct return the solution ands its index in the map, and keep goign to the array
+untill you reach the end 
+
+if the elements are not correct it will keep going to the end of the array to find right solution of the index 
+
+
+
+
+
+time complexity: 0(n)
+
+*/
